12d56842f2340e7abdaac4fd034c6c061e351426
Naoya Fujio <naofudiwo@gmail.com>
2020-08-25 16:14:29 +0000

    apply seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@admin.com', password: '7218099', password_confirmation: '7218099') if Rails.env.development?
+AdminUser.create!(email: 'admin@admin.com', password: '7218099', password_confirmation: '7218099')
+30.times do |n|
+    user = User.new(
+      email: "#{n + 1}@test.com",
+      username: "テスト#{n + 1}",
+      password: "13549#{n + 1}",
+      password_confirmation: "13549#{n + 1}"
+    )
+    user.save!
+end
