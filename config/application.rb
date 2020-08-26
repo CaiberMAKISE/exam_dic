3f0f6cb4141692205a4bc75c3999faa204771ac9
Naoya Fujio <naofudiwo@gmail.com>
2020-08-26 08:07:22 +0000

    install rspec

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,16 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.generators do |g|
+  g.test_framework :rspec,
+                   model_specs: true,
+                   view_specs: false,
+                   helper_specs: false,
+                   routing_specs: false,
+                   controller_specs: false,
+                   request_specs: false
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
---
+++ spec/system/post.spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+RSpec.describe '', type: :system do
+  describe '' do
+    context '' do
+      it '' do
+      end
+    end
+  end
+end
